0f8b1d80bb587f3cb07c47d0bf308d01c0edfcac
brfialho <brfialho@student.42sp.org.br>
2025-09-27 00:28:31 -0300

    done almost all validations// no leaks// missing repeated numbers

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 16:53:49 by brfialho          #+#    #+#             */
-/*   Updated: 2025/09/26 22:17:09 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/09/27 00:25:44 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-void	error_handler(t_list **head, char **split)
+void	error_handler(t_list **head, char **split, char *argv)
 {
 	if (head)
 	{
@@ -22,6 +22,8 @@ void	error_handler(t_list **head, char **split)
 	}
 	if (split)
 		ft_split_free(split);
+	if (argv)
+		free(argv);
 	ft_printf("Error\n");
 	exit(1);
 }
@@ -52,6 +54,29 @@ long	push_atol(char *s)
 	return (sum * sign);
 }
 
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*join;
+	char	*tmp;
+	size_t	len;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	join = ft_calloc(1, len + 1);
+	if (!join)
+		return (ft_free(&s1), ft_free(&s2), NULL);
+	tmp = s1;
+	if (tmp)
+		while (*tmp)
+			*join++ = *tmp++;
+	tmp = s2;
+	if (tmp)
+		while (*tmp)
+			*join++ = *tmp++;
+	ft_free(&s1);
+	ft_free(&s2);
+	return (join - len);
+}
+
 int	append_node(char *input, t_list **head)
 {
 	t_list	*new;
@@ -80,28 +105,70 @@ void	split_input(char *s, t_list **head)
 	i = 0;
 	split = ft_split(s, ' ');
 	if (!split)
-		error_handler(head, NULL);
+		error_handler(head, NULL, s);
 	while (split[i])
 		if(!append_node(split[i++], head))
-			error_handler(head, split);
+			error_handler(head, split, s);
+	free(s);
 	ft_split_free(split);
 }
 
+void remove_newline(char *s)
+{
+	int	i;
+
+	i = -1;
+	while (s[++i])
+		if (s[i] == '\n')
+			s[i] = ' ';
+}
+
+t_list	*get_stdin_input(t_list **head)
+{
+	char	*input;
+	char	*buffer;
+	t_list	*list;
+
+	input = 0;
+	buffer = get_next_line(STDIN_FILENO);
+	if (!buffer)
+		error_handler(head, NULL, NULL);
+	remove_newline(buffer);
+	while (buffer)
+	{
+		input = ft_strjoin_free(input, buffer);
+		if (!input)
+			error_handler(head, NULL, NULL);
+		buffer = get_next_line(STDIN_FILENO);
+		if (buffer)
+			remove_newline(buffer);
+	}
+	split_input(input, head);
+	list = *head;
+	free(head);
+	return (list);
+}
 
 t_list	*get_list(int argc, char* argv[])
 {
 	t_list	**head;
 	t_list	*list;
+	char	*s;
 	int		i;
 
-	if (argc < 2)
-		error_handler(NULL, NULL);
 	i = 1;
 	head = ft_calloc(1, sizeof(t_list**));
 	if (!head)
-		error_handler(NULL, NULL);
+		error_handler(NULL, NULL, NULL);
+	if (argc < 2)
+		return (get_stdin_input(head));
 	while (i < argc)
-		split_input(argv[i++], head);
+	{
+		s = ft_strdup(argv[i++]);
+		if (!s)
+			error_handler(head, NULL, NULL);
+		split_input(s, head);
+	}
 	list = *head;
 	free(head);
 	return (list);
@@ -115,14 +182,23 @@ int	main(int argc, char *argv[])
 
 	head = get_list(argc, argv);
 	aux = head;
+	printf("\n");
 	while (aux)
 	{
 		printf("%ld\n", *((long *)aux->content));
 		aux = aux->next;
 	}
 	ft_lstclear(&head, free);
+	
 	(void)argc;
 	(void)argv;
+	// char *str=NULL;
+	// char *tmp;
+	// while ((tmp = get_next_line(STDIN_FILENO)))
+	// 	str = ft_strjoin_free(str, tmp);
+	// 	// ft_printf("%d\n", tmp);
+	// ft_printf("%s\n", str);
+	// free(str);
 }
 
 //TO DO 
