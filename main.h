bf0c7876b11d4e682713681fc85afe7b6be0079c
brfialho <brfialho@student.42sp.org.br>
2025-09-30 03:07:16 -0300

    changes in lib/header

--- main.h
+++ main.h
@@ -6,20 +6,14 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 16:55:02 by brfialho          #+#    #+#             */
-/*   Updated: 2025/09/30 00:10:50 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/09/30 03:06:02 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
 # define MAIN_H
 
-# ifndef INT_MAX
-#  define INT_MAX 2147483647
-# endif
-
-# ifndef INT_MIN
-#  define INT_MIN -2147483648
-# endif
+# include "libft/headers/libft.h"
 
 struct s_number {
 	long	number;
@@ -27,6 +21,5 @@ struct s_number {
 } typedef t_number;
 
 
-#include "libft/headers/libft.h"
 
 #endif
